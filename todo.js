383ae2fc6a4f309d05baca00af793610736cb653
Eamonn de Leastar <edeleastar@wit.ie>
2022-03-25 10:52:59 +0000

    date introduced

--- todo.js
+++ todo.js
@@ -5,12 +5,15 @@ function renderTodo(todo) {
   const row = table.insertRow(-1);
   const textCell = row.insertCell(0);
   textCell.innerText = todo.text;
+  const dateCell = row.insertCell(1);
+  dateCell.innerText = todo.date;
 }
 
 function addTodo() {
   const todoText = document.getElementById("todo-id").value;
   const todo = {
-    text: todoText
+    text: todoText,
+    date: new Date().toLocaleString("en-IE"),
   };
   todoItems.push(todo);
   renderTodo(todo);
