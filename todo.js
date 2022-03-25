dfc54108b92b675d20d45029c8c7acd69be17254
Eamonn de Leastar <edeleastar@wit.ie>
2022-03-25 10:46:46 +0000

    refactored addTodo

--- todo.js
+++ todo.js
@@ -1,14 +1,17 @@
 let todoItems = [];
 
-let count = 0;
-
-function addTodo() {
-  count++;
-  const todoText = document.getElementById("todo-id").value;
-  console.log(`add todo call ${count}: ${todoText}`)
-
+function renderTodo(todo) {
   const table = document.getElementById("todo-table");
   const row = table.insertRow(-1);
   const textCell = row.insertCell(0);
-  textCell.innerText = todoText;
+  textCell.innerText = todo.text;
+}
+
+function addTodo() {
+  const todoText = document.getElementById("todo-id").value;
+  const todo = {
+    text: todoText
+  };
+  todoItems.push(todo);
+  renderTodo(todo);
 }
