e8d268aa4189325046ff20407e2e0e38020e673a
Eamonn de Leastar <edeleastar@wit.ie>
2022-03-25 10:42:11 +0000

    display todos in table

--- todo.js
+++ todo.js
@@ -6,4 +6,9 @@ function addTodo() {
   count++;
   const todoText = document.getElementById("todo-id").value;
   console.log(`add todo call ${count}: ${todoText}`)
+
+  const table = document.getElementById("todo-table");
+  const row = table.insertRow(-1);
+  const textCell = row.insertCell(0);
+  textCell.innerText = todoText;
 }
